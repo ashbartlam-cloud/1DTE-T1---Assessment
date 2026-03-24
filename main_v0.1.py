d8ba1f43df79b65b4709cf9e35ed22bd1107ad96
ashbartlam-cloud <ash.bartlam@papamoacollege.school.nz>
2026-03-24 20:02:58 +1300

    v0.01
    Started coding the game, made the code work. Block 1/7 completed and I started with the equation variables and making sure they are prepared for next block

---
+++ main_v0.1.py
@@ -0,0 +1,37 @@
+import random
+
+# Used in roll it 13, this is for my instructions at the start of the game getting the users input
+def yes_no(question):
+    question = "- Do you want instructions? -"
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please Enter Yes Or No!")
+
+# Define the instructions for the start of the game
+def instructions():
+    print("""
+==== Instructions ====
+
+Your goal is simple, solve the equations given and collect points,
+one correct gives one point and one wrong gives a negative point.
+Good Luck!
+    """)
+
+# Make a title to print at the start of every game
+title = "=== Basic Math Quest ==="
+print(title)
+print()
+
+# Asking the user if they would like instructions and checking whether they said yes or no
+ask_instructions = yes_no('- Do you want instructions? - ')
+
+if ask_instructions == "yes":
+    instructions()
+
+# Checks the users input
---
+++ main_v0.2.py
@@ -0,0 +1,54 @@
+import random
+
+# VARIABLES ================================================================================
+
+# clarify my the lists for the numbers in the equation
+numerator = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+denominator = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# select a random number from the list
+numerator_random = random.choice(numerator)
+denominator_random = random.choice(numerator)
+
+# and finally put them together (change the + to a list that is chosen as random)
+equation = f"{numerator_random} + {denominator_random}"
+
+# INSTRUCTIONS & START SECTION =============================================================
+
+# used in roll it 13, this is for my instructions at the start of the game getting the users input
+def yes_no(question):
+    question = "- Do you want instructions? -"
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please Enter Yes Or No!")
+
+# define the instructions for the start of the game
+def instructions():
+    print("""
+==== Instructions ====
+
+Your goal is simple, solve the equations given and collect points,
+one correct gives one point and one wrong gives a negative point.
+Good Luck!
+    """)
+
+# make a title to print at the start of every game
+title = "=== BASIC MATH QUEST ==="
+print(title)
+print()
+
+# executing the instructions and asking the user for an input (from yes_no func)
+ask_instructions = yes_no('- Do you want instructions? - ')
+
+# if the user has put yes or y then it will come back with instructions
+if ask_instructions == "yes":
+    instructions()
+
+# ==============================================================================
+
